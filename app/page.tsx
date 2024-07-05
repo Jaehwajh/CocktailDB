98dd340fd6c1711b4d1ad643c39dab589b61a297
Jaehwa <Jaehwa.jh@gmail.com>
2024-07-04 18:13:43 -0700

    header

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
       <div className='flex flex-col pb-[2rem] gap-y-[2rem] lg:gap-y-[1rem]'>
         <p className='text-6xl font-black text-center'>Cocktail Database</p>
         <p className='text-2xl font-medium text-center px-[4rem] lg:text-lg lg:font-light'>Please input the type of alcoholic drink you want. If you want to see a wider selection of a particular alcoholic drink then input the alcohol base. (eg. Vodka, Gin, Rum etc..)</p>
+        <div className='flex flex-col py-1'>
+          <p className='text-sm font-light text-center italic'>This application uses the Drinks API; some instructions, ingredients, and/or measurements may be written incorrectly so please make the best out of the drink details. You can always play with the ratios to make it fit your pallet.</p>
+          <p className='text-sm font-bold text-center italic'>Never drink and drive and always drink responsibly. Happy Drinking!</p>
+        </div>
         <SearchDrinks/>
       </div>
     </main>
