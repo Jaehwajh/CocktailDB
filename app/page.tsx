2c1df427b14344fbdef745909593e9fe1b5cbd44
Jaehwa <Jaehwa.jh@gmail.com>
2024-07-07 13:54:59 -0700

    icons and style changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center pt-[4rem] lg:pt-[6rem]">
       <div className='flex flex-col pb-[2rem] gap-y-[2rem] lg:gap-y-[1rem]'>
-        <p className='text-6xl font-black text-center'>Cocktail Database</p>
+        <div className='flex justify-center gap-x-2'>
+          <Image
+            src='/assets/gif.webp'
+            width={80}
+            height={80}
+            alt="client work pksan"
+            loading="lazy"
+            className=""
+            unoptimized={true}
+          />
+          <p className='text-6xl font-black text-center my-auto'>Cocktail Database</p>
+        </div>
         <p className='text-2xl font-medium text-center px-[4rem] lg:text-lg lg:font-light'>Please input the type of alcoholic drink you want. If you want to see a wider selection of a particular alcoholic drink then input the alcohol base. (eg. Vodka, Gin, Rum etc..)</p>
         <div className='flex flex-col py-1'>
           <p className='text-sm font-light text-center italic px-[4rem]'>This application uses the Drinks API; some instructions, ingredients, and/or measurements may be written incorrectly so please make the best out of the drink details. You can always play with the ratios to make it fit your pallet.</p>
